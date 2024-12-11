b88d7a6f2816b9433cb429b7b7ddfa98b9625c67
Emmanuel De Luca <emmanueldelucca009@gmail.com>
2024-12-11 16:27:55 -0300

    Refactory components

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import "./styles.css"
-export class Article extends React.Component {
-    render() {
-        return (
-            <article id="article">
-                <img src={this.props.imageNoticia} alt={this.props.title} />
-                <div className="article-infos">
-                    <h2>{this.props.title}</h2>
-                    <h3>{this.props.provider}</h3>
-                    <p>
-                        {this.props.description}
-                    </p>
-                </div>
-            </article>
-        )
-    }
+import "./styles.css";
 
+export function Article({title, provider, description, imageNoticia}) {
+  return (
+    <article id="article">
+      <img src={imageNoticia} alt={title} />
+      <div className="article-infos">
+        <h2>{title}</h2>
+        <h3>{provider}</h3>
+        <p>{description}</p>
+      </div>
+    </article>
+  );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./styles.css";
 
-export class Footer extends React.Component {
-  render() {
-    return (
-      <div id="footer">
-        <p className="footer-conteudo">&copy; Todos os direitos reservados</p>
-      </div>
-    );
-  }
+export function Footer() {
+  return (
+    <div id="footer">
+      <p className="footer-conteudo">&copy; Todos os direitos reservados</p>
+    </div>
+  );
 }
