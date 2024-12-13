6ffce3c8e46f727fb77ed3c2d933fb979f7cf14f
Emmanuel De Luca <emmanueldelucca009@gmail.com>
2024-12-13 14:06:42 -0300

    Refactory Counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,53 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class Counter extends React.Component {
-  constructor() {
-    super();
-    //this.contador = 0;
-    this.state = { contador: 0, name: "", password: "" };
-  }
+export function Counter() {
+  const [contador, setContador] = useState(0)
+  return (
+    <div style={{ marginTop: "20px", marginLeft: "20px" }}>
+      <h1>{contador}</h1>
+      <div>
+        <button
+          onClick={() => {
+            setContador(contador - 1);
+          }}
+        >
+          Diminuir
+        </button>
 
-  render() {
-    return (
-      <div style={{ marginTop: "20px", marginLeft: "20px" }}>
-        <h1>{this.state.contador}</h1>
-        <div>
-          <button
-            onClick={() => {
-              this.setState({ contador: this.state.contador - 1 });
-            }}
-          >
-            Diminuir
-          </button>
-
-          <button
-            onClick={() => {
-              this.setState({ contador: this.state.contador + 1 });
-            }}
-          >
-            Aumentar
-          </button>
-        </div>
-        <form action="" style={{ marginTop: "40px" }}>
-          <input
-            type="text"
-            placeholder="Nome"
-            value={this.state.name}
-            onChange={(event) => {
-              this.setState({ name: event.target.value });
-            }}
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            value={this.state.password}
-            onChange={(event) => {
-              this.setState({ password: event.target.value });
-            }}
-          />
-          <button>Enviar</button>
-        </form>
+        <button
+          onClick={() => {
+            setContador(contador + 1);
+          }}
+        >
+          Aumentar
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
